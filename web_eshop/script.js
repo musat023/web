5eacd24c4d630935263b5f9b2a3cfa1aa07f021a
musat023 <145437556+musat023@users.noreply.github.com>
2025-04-02 11:57:40 +0200

    Update script.js

--- web_eshop/script.js
+++ web_eshop/script.js
@@ -6,3 +6,26 @@ document.addEventListener("DOMContentLoaded", function () {
         navMenu.classList.toggle("active");
     });
 });
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var message = document.getElementById("message");
+
+    if (username === "musat" && password === "isik") {
+        message.textContent = "Login completed!";
+        message.style.color = "green";
+    } else {
+        message.textContent = "Wrong username or password!";
+        message.style.color = "red";
+    }
+});
+
+document.getElementById("openLogin").addEventListener("click", function() {
+   
+    document.getElementById("loginContainer").style.display = "block";
+});
+
+
